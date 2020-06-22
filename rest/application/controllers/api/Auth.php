ea184d9fa16fde6d92600ba4e1e9fad943e3d79c
nadahasnim <nadasthing@gmail.com>
2020-06-22 19:43:47 +0700

    menamabah REST/API/FORGOTPASSWORD
    Api endpoints untuk melakukan proses lupa password android

--- rest/application/controllers/api/Auth.php
+++ rest/application/controllers/api/Auth.php
@@ -1,6 +1,8 @@
 <?php
+
 use Restserver\Libraries\REST_Controller;
-defined('BASEPATH') OR exit('No direct script access allowed');
+
+defined('BASEPATH') or exit('No direct script access allowed');
 
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
 /** @noinspection PhpIncludeInspection */
@@ -19,9 +21,9 @@ require APPPATH . 'libraries/Format.php';
  * @license         MIT
  * @link            https://github.com/chriskacerguis/codeigniter-restserver
  */
-class Auth extends CI_Controller {
-    use REST_Controller 
-    {
+class Auth extends CI_Controller
+{
+    use REST_Controller {
         REST_Controller::__construct as private __resTraitConstruct;
     }
 
@@ -48,31 +50,31 @@ class Auth extends CI_Controller {
         $userData = $this->db->get_where('pengguna', ['email_pengguna' => $email])->row_array();
 
         // apabila email ada
-        if($userData){
+        if ($userData) {
             // apabila belum aktifasi
-            if($userData['is_active'] == 1){
+            if ($userData['is_active'] == 1) {
                 // cek password
-                if(password_verify($password, $userData['password'])){      
-                    $data = [     
+                if (password_verify($password, $userData['password'])) {
+                    $data = [
                         'id_pengguna' => $userData['id_pengguna'],
                         'nama_pengguna' => $userData['nama_pengguna'],
                         'alamat_pengguna' => $userData['alamat_pengguna'],
                         'provinsi_pengguna' => $userData['provinsi_pengguna'],
                         'kota_pengguna' => $userData['kota_pengguna'],
                         'telepon_pengguna' => $userData['telepon_pengguna'],
-                        'email_pengguna' => $userData['email_pengguna'],                                         
-                        'kelamin_pengguna' => $userData['kelamin_pengguna'],                                         
-                        'tanggal_lahir_pengguna' => $userData['tanggal_lahir_pengguna'],                                         
-                        'no_ktp_pengguna' => $userData['no_ktp_pengguna'],                                         
-                        'foto_pengguna' => 'http://localhost/indiekostci/assets/img/'.$userData['foto_pengguna'],                                                                                           
+                        'email_pengguna' => $userData['email_pengguna'],
+                        'kelamin_pengguna' => $userData['kelamin_pengguna'],
+                        'tanggal_lahir_pengguna' => $userData['tanggal_lahir_pengguna'],
+                        'no_ktp_pengguna' => $userData['no_ktp_pengguna'],
+                        'foto_pengguna' => 'http://localhost/indiekostci/assets/img/' . $userData['foto_pengguna'],
                         'id_akses' => $userData['id_akses']
-                    ];          
-                    
+                    ];
+
                     $this->response([
                         'status' => true,
                         'message' => 'Login Berhasil',
                         'data' => $data
-                    ], 200);                    
+                    ], 200);
                 } else {
                     $this->response([
                         'status' => false,
@@ -83,6 +85,90 @@ class Auth extends CI_Controller {
                 $this->response([
                     'status' => false,
                     'message' => 'Akun belum diaktivasi'
+                ], 401);
+            }
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Email tidak ditemukan'
+            ], 401);
+        }
+    }
+
+
+
+
+    // fungsi proses send email
+    private function _sendEmail($token, $email, $type)
+    {
+        $config = [
+            'protocol'      => 'smtp',
+            'smtp_host'     => 'smtp.gmail.com',
+            'smtp_user'     => 'indiekostteknotirta@gmail.com',
+            'smtp_pass'     => 'indieforever',
+            'smtp_port'     => 465,
+            'mailtype'      => 'html',
+            'charset'       => 'utf-8',
+            'smtp_crypto'   => 'ssl',
+            'crlf'          => "\r\n",
+            'newline'       => "\r\n"
+        ];
+
+        $this->load->library('email', $config);
+        $this->email->initialize($config);
+
+        $this->email->from('indiekostteknotirta@gmail.com', 'INDIEKOST');
+        $this->email->to($email);
+
+        if ($type == 'verify') {
+            $message = 'Klik link berikut untuk melakukan aktivasi akun anda <a href="' . base_url("auth/verify") . '?email=' . $email . '&token=' . $token . '">AKTIVASI AKUN</a>';
+
+            $this->email->subject('Verifikasi Akun INDIEKOST');
+            $this->email->message($message);
+        } else if ($type == 'forgot') {
+            $message = 'Klik link berikut untuk mengatur ulang password akun anda <a href="' . base_url("auth/resetpassword") . '?email=' . $email . '&token=' . $token . '">RESET PASSWORD</a>';
+
+            $this->email->subject('Reset Password Akun INDIEKOST');
+            $this->email->message($message);
+        }
+
+        if ($this->email->send()) {
+            return true;
+        } else {
+            echo $this->email->print_debugger();
+            die;
+        }
+    }
+
+    public function forgotPassword_post()
+    {
+        if ($this->post('email') && $this->post('base_url')) {
+            // jika ada email
+            $email = $this->post('email');
+
+            $user = $this->db->get_where('pengguna', ['email_pengguna' => $email, 'is_active' => 1])->row_array();
+
+            if ($user) {
+                $token = uniqid(true);
+
+                $user_token = [
+                    'email'         => $email,
+                    'token'         => $token,
+                    'date_created'  => time()
+                ];
+
+                $this->db->insert('user_token', $user_token);
+
+                $this->_sendEmail($token, $email, 'forgot');
+
+                $this->response([
+                    'status' => true,
+                    'message' => 'Berhasil mengirim email reset password'
+                ], 200);
+            } else {
+                $this->response([
+                    'status' => false,
+                    'message' => 'Email tidak terdaftar atau belum aktif'
                 ], 401);
             }
         } else {
